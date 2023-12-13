60819b94dfba80c2f836d5bb2d34f645cdb4d65d
Roshan-Kumar-Mishra <144347812+Roshan-Kumar-Mishra@users.noreply.github.com>
2023-12-13 18:32:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,40 @@
-// function shortcut(s1, s2) {
-//   // your code here
-// }
-
-// // Do not change the code below.
-// const s1 = prompt("Enter s1:");
-// const s2 = prompt("Enter s2:");
-// alert(shortcut(s1, s2));
-function shortcut(str1, str2) {
+function shortcut(s1, s2) {
+  // your code here
+	function shortcut(s1, s2) {
     // Check if either string is empty
-    if (!str1 || !str2) {
+    if (!s1 || !s2) {
         return "";
     }
 
     // Return the initial letters of the strings
-    return str1[0] + str2[0];
+    return s1[0] + s2[0];
 }
 
 // Example usage:
 // const string1 = "hello";
 // const string2 = "world";
-const result = shortcut(string1, string2);
+const result = shortcut(s1, s2);
 console.log(result);
+
+}
+
+// Do not change the code below.
+const s1 = prompt("Enter s1:");
+const s2 = prompt("Enter s2:");
+alert(shortcut(s1, s2));
+// function shortcut(str1, str2) {
+//     // Check if either string is empty
+//     if (!str1 || !str2) {
+//         return "";
+//     }
+
+//     // Return the initial letters of the strings
+//     return str1[0] + str2[0];
+// }
+
+// // Example usage:
+// // const string1 = "hello";
+// // const string2 = "world";
+// const result = shortcut(string1, string2);
+// console.log(result);
 
