3b573145d816ccc9c31a58757ce959b4b8eb5d71
Roshan-Kumar-Mishra <144347812+Roshan-Kumar-Mishra@users.noreply.github.com>
2023-12-13 18:26:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-function shortcut(s1, s2) {
-  // your code here
+// function shortcut(s1, s2) {
+//   // your code here
+// }
+
+// // Do not change the code below.
+// const s1 = prompt("Enter s1:");
+// const s2 = prompt("Enter s2:");
+// alert(shortcut(s1, s2));
+function shortcut(str1, str2) {
+    // Check if either string is empty
+    if (!str1 || !str2) {
+        return "";
+    }
+
+    // Return the initial letters of the strings
+    return str1[0] + str2[0];
 }
 
-// Do not change the code below.
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
-alert(shortcut(s1, s2));
+// Example usage:
+// const string1 = "hello";
+// const string2 = "world";
+const result = shortcut(string1, string2);
+console.log(result);
+
